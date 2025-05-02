987315ebd22e93852d4b6a34cc5416bbc83006fe
PhillipThomas1992 <phillipthomas@live.de>
2025-05-02 12:02:03 +0200

    added endpoints for model name and url

--- app/main.py
+++ app/main.py
@@ -1,6 +1,9 @@
+import json
 import logging
+import os
 from datetime import datetime, timezone
 
+from make87_messages.core.empty_pb2 import Empty
 from make87_messages.core.header_pb2 import Header
 from make87_messages.text.text_plain_pb2 import PlainText
 from make87_messages.image.compressed.image_jpeg_pb2 import ImageJPEG
@@ -17,6 +20,18 @@ def main():
     client = Client()
     client.pull(model=model_name, stream=False)
     logging.info(f"Model {model_name} downloaded.")
+
+    vpn_ip = os.environ.get("VPN_IP", None)
+    port_config = os.environ.get("PORT_CONFIG", None)
+    vpn_server_url = None
+    if vpn_ip is None or port_config is None:
+        # search for OLLAMA and get public port
+        port_config = json.loads(port_config)
+        for port_config in port_config:
+            if port_config["name"] == "OLLAMA":
+                port = port_config["published_port"]
+                vpn_server_url = f"http://{vpn_ip}:{port}"
+                break
 
     endpoint = make87.get_provider(
         name="CHAT", requester_message_type=PlainText, provider_message_type=PlainText
@@ -53,6 +68,28 @@ def main():
         )
 
     endpoint.provide(callback_img)
+
+    vpn_url_endpoint = make87.get_provider(
+        name="VPN_SERVER_URL", requester_message_type=Empty, provider_message_type=PlainText
+    )
+    def callback_vpn_url(message: Empty) -> PlainText:
+        return PlainText(
+            header=make87.header_from_message(Header, message=message, append_entity_path="response"),
+            body=vpn_server_url,
+        )
+
+    if vpn_server_url is not None:
+        vpn_url_endpoint.provide(callback_vpn_url)
+
+    model_name_endpoint = make87.get_provider(
+        name="MODEL_NAME", requester_message_type=Empty, provider_message_type=PlainText
+    )
+    def callback_model_name(message: Empty) -> PlainText:
+        return PlainText(
+            header=make87.header_from_message(Header, message=message, append_entity_path="response"),
+            body=model_name,
+        )
+    model_name_endpoint.provide(callback_model_name)
 
     make87.loop()
 
