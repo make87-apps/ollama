f9c2f0c889c65aa6c16f377858a374fe2975f532
PhillipThomas1992 <phillipthomas@live.de>
2025-08-22 11:41:56 +0200

    updated ollama verison and added rocm variant

--- app/main.py
+++ app/main.py
@@ -10,17 +10,20 @@ logging.basicConfig(level=logging.INFO)
 
 def main():
     config = m87.config.load_config_from_env()
-    model_name = m87.config.get_config_value(config, "model_name", default="qwen3:14b")
-    logger.info(f"Using model {model_name}")
+    model_name = m87.config.get_config_value(config, "model_name", default=None)
+    if model_name is not None:
+        logger.info(f"Using model {model_name}")
 
-    logger.info("Setup vpn and model name endpoints")
+        logger.info("Setup vpn and model name endpoints")
 
-    logger.info(f"Downloading model {model_name}...")
-    client = Client()
-    client.pull(model=model_name, stream=False)
-    logger.info(f"Model {model_name} downloaded.")
+        logger.info(f"Downloading model {model_name}...")
+        client = Client()
+        client.pull(model=model_name, stream=False)
+        logger.info(f"Model {model_name} downloaded.")
 
-    logger.info("Ollama is ready.")
+        logger.info("Ollama is ready.")
+    else:
+        logger.info(f"No model passed. Skipping download.")
 
     m87.run_forever()
 
